233eef30b2021549723847bd5b404f652d62eebe
Alina Salinas Mendoza <alinasalmen995@gmail.com>
2018-11-19 15:40:21 -0600

    Arreglando la obtencion de datos

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,21 @@
 const SerialPort = require('serialport');
 
-const parser = SerialPort.parsers.Readline;
+const Readline = SerialPort.parsers.Readline;
 
-const port = new SerialPort('COM3',{
+const port = new SerialPort('COM4',{
     baudRate: 9600
 });
 
-const parser = port.pipe(new ReadLine({ delimiter: '\r\n' }));
+const parser = port.pipe(new Readline({ delimeter: '\r\n' }));
 
 parser.on('open', function(){
     console.log('connection is opened');
 });
 
-parser.on('data',function(){
+parser.on('data',function(data){
     console.log(data);
 });
 
 port.on('error', function(err){
-    console.log(err);
+    console.log('Lol: '+ err);
 });
