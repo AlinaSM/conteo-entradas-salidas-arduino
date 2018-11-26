48af94002089c916444d9b4d8bb88ef422c337df
Alina Salinas Mendoza <alinasalmen995@gmail.com>
2018-11-25 21:21:20 -0600

    Modificando la adquisicion de datos

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,28 @@
+// This script is released to the public domain and may be used, modified and
+// distributed without restrictions. Attribution not necessary but appreciated.
+// Source: https://weeknumber.net/how-to/javascript
+
+// Returns the ISO week of the date.
+Date.prototype.getThisWeek = function(thisTime = "") {
+    if(thisTime == ""){
+        var date = new Date(this.getTime());
+        date.setHours(0, 0, 0, 0);
+    }else{
+        var date = new Date(thisTime);
+        date.setHours(0, 0, 0, 0);
+    }
+    
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    // January 4 is always in week 1.
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                          - 3 + (week1.getDay() + 6) % 7) / 7);
+  }
+
+  
+
 //Conexion a mySql
 var mysql = require('mysql');
 
@@ -5,7 +30,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'arduino-puerta'
+    database: 'db_puerta_arduino'
 });
 
 connection.connect(function(error){
@@ -20,7 +45,7 @@ connection.connect(function(error){
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-var date = new Date();
+
 
 const app = express();
 const server = http.createServer(app);
@@ -49,24 +74,23 @@ parser.on('open', function(){
 });
 
 parser.on('data',function(data){
-    let tiempo = new Date();
-
+    let tiempo = new Date(); 
     let diasDelaSemana = ['Lunes','Martes', 'Miercoles', 'Jueves','Viernes', 'Sabado'];
     let mesesDelAnio = ['Enero','Febrero', 'Marzo', 'Abril','Mayo', 'Junio',
                         'Julio','Agosto', 'Septiembre', 'Octubre','Noviembre', 'Diciembre'];
     
     let sql = "INSERT INTO `puerta` (`estado`, `dia_semana`, `dia`, `mes`, `anio`, `hora`, `minuto`,"+
-              " `segundo`) VALUES ('"+data+"', '"+ diasDelaSemana[tiempo.getDay() - 1] +
+              " `segundo`, `numero_semana`) \n VALUES ('"+data+"', '"+ diasDelaSemana[tiempo.getDay() - 1] +
               "', "+tiempo.getDate()+", '"+ mesesDelAnio[tiempo.getMonth()] + "',"+tiempo.getFullYear()+", "+
-              ( tiempo.getHours() + 1 )+", "+ tiempo.getMinutes() +", "+ tiempo.getSeconds() +");";
-    
+              ( tiempo.getHours() + 1 )+", "+ tiempo.getMinutes() +", "+ tiempo.getSeconds() +","+tiempo.getThisWeek()+");";
+              
     connection.query(sql, function(error, result){
         if(error)
             throw error;
-        console.log('Dato insertado: '+ data);
+        //console.log('Dato insertado: '+ tiempo.getThisWeek(mes+" "+6+", "+2018+" 0:0:0"));
     });
     
-    //io.emit('sql', sql);
+    io.emit('sql', sql);
     
 });
 
